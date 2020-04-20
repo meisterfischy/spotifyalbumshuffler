8bc5b8910997b6397f1ade07792fdb2b470966ee
meisterfischy <Konstantin.Grabmann@gmail.com>
2020-04-20 22:01:23 +0200

    Spotify does not love me

--- guenter.php
+++ guenter.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['access_token']) && !isset($_SESSION['refresh_token']) && !
     $_SESSION['exp_date'] = 0;
 }
 
-print_r(getAlbums('2JN9ZjnLh182Dak5rIazyP'));
+play('2JN9ZjnLh182Dak5rIazyP');
 
 function token() {
 
@@ -74,7 +74,35 @@ function getAlbums($play_id) {
 
     }
 
-    return $ids;
+    return $ids -> toArray();
+
+}
+
+function getSongs($alb_id) {
+
+    $url = 'https://api.spotify.com/v1/albums/'.$alb_id.'/tracks?limit=50';
+    $headers = array(
+        'Accept: application/json',
+        'Content-Type: application/json',
+        'Authorization: Bearer '.token()
+    );
+
+    $ch = curl_init();
+    curl_setopt_array($ch, [
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_HTTPHEADER => $headers,
+        CURLOPT_URL => $url
+    ]);
+
+    $response = json_decode(curl_exec($ch), true);
+    curl_close($ch);
+
+    $songs = array();
+
+    for($index = 0; $index < $response['total']; $index++) 
+        array_push($songs, $response['items'][$index]['uri']);
+
+    return $songs;
 
 }
 
@@ -98,6 +126,58 @@ function getTotalSongs($play_id) {
     curl_close($ch);
 
     return $response['total'];
+
+}
+
+function albumShuffle(array $ary) {
+
+    for($index = (count($ary) - 1); $index > 0; $index--) {
+
+        $rdm = rand(0, $index);
+        $index -=1;
+
+        $temp = $ary[$index];
+        $ary[$index] = $ary[$rdm];
+        $ary[$rdm] = $temp;
+
+    }
+
+    return $ary;
+
+}
+
+function play($play_id) {
+
+    $albums = albumShuffle(getAlbums('2JN9ZjnLh182Dak5rIazyP'));
+
+    #foreach($albums as $alb) {
+        #$songs = getSongs($alb);
+        $songs = getSongs($albums[0]);
+        foreach($songs as $song) {
+
+            print_r($song);
+
+            $url = 'https://api.spotify.com/v1/me/player/queue?uri='.urlencode($song);
+            $headers = array(
+                'Accept: application/json',
+                'Content-Type: application/json',
+                'Authorization: Bearer '.token()
+            );
+
+            $ch = curl_init();
+            curl_setopt_array($ch, [
+                CURLOPT_HTTPHEADER => $headers,
+                CURLOPT_URL => $url,
+                CURLOPT_POST => 1,
+                CURLOPT_POSTFIELDS => 'uri='.urlencode($song)
+            ]);
+
+            curl_exec($ch);
+            curl_close($ch);
+
+        }
+
+    #}
 
 }
 
